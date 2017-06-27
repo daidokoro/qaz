41fcff18a18b72afef98fe65473ec558cdc30427
Shaun <shaunremekie@shauns-mbp.home>
2017-06-27 01:03:54 +0100

    updated shell commands, added check, update, deploy, terminate, generate

--- commands/shell.go
+++ commands/shell.go
@@ -3,12 +3,15 @@ package commands
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"qaz/utils"
 	"regexp"
+	"strconv"
+	"strings"
 
 	yaml "gopkg.in/yaml.v2"
 
-	"github.com/abiosoft/ishell"
+	"github.com/daidokoro/ishell"
 	stks "github.com/daidokoro/qaz/stacks"
 	"github.com/spf13/cobra"
 )
@@ -79,11 +82,12 @@ func initShell(p string, s *ishell.Shell) {
 
 		// outputs command
 		&ishell.Cmd{
-			Name: "outputs",
-			Help: "Prints stack outputs",
+			Name:     "outputs",
+			Help:     "Prints stack outputs",
+			LongHelp: "outputs [stack]",
 			Func: func(c *ishell.Context) {
 				if len(c.Args) < 1 {
-					fmt.Println("Please specify stack(s) to check, For details try --> qaz outputs --help")
+					log.Warn("Please specify stack(s) to check")
 					return
 				}
 
@@ -119,11 +123,13 @@ func initShell(p string, s *ishell.Shell) {
 
 		// values command
 		&ishell.Cmd{
-			Name: "values",
-			Help: "print stack values from config in YAML format",
+			Name:     "values",
+			Help:     "print stack values from config in YAML format",
+			LongHelp: "values [stack]",
 			Func: func(c *ishell.Context) {
-				if len(c.Args) == 0 {
-					utils.HandleError(fmt.Errorf("Please specify stack name..."))
+
+				if len(c.Args) < 1 {
+					log.Warn(fmt.Sprintf("Please specify stack name..."))
 					return
 				}
 
@@ -132,6 +138,7 @@ func initShell(p string, s *ishell.Shell) {
 
 				if _, ok := stacks[s]; !ok {
 					log.Error(fmt.Sprintf("Stack [%s] not found in config", s))
+					return
 				}
 
 				values := stacks[s].TemplateValues[s].(map[string]interface{})
@@ -140,11 +147,13 @@ func initShell(p string, s *ishell.Shell) {
 				output, err := yaml.Marshal(values)
 				if err != nil {
 					log.Error(err.Error())
+					return
 				}
 
 				reg, err := regexp.Compile(".+?:(\n| )")
 				if err != nil {
 					log.Error(err.Error())
+					return
 				}
 
 				resp := reg.ReplaceAllStringFunc(string(output), func(s string) string {
@@ -152,6 +161,239 @@ func initShell(p string, s *ishell.Shell) {
 				})
 
 				fmt.Printf("\n%s\n", resp)
+			},
+		},
+
+		// deploy command
+		&ishell.Cmd{
+			Name: "deploy",
+			Help: "Deploys stack(s) to AWS",
+			Func: func(c *ishell.Context) {
+				run.stacks = make(map[string]string)
+				// stack list
+				stklist := make([]string, len(stacks))
+				i := 0
+				for k := range stacks {
+					stklist[i] = k
+					i++
+				}
+
+				// create checklist
+				choices := c.Checklist(
+					stklist,
+					fmt.Sprintf("select stacks to %s:", log.ColorString("Deploy", "cyan")),
+					nil,
+				)
+
+				// define run.stacks
+				run.stacks = make(map[string]string)
+				for _, i := range choices {
+					if i < 0 {
+						fmt.Printf("--\nPress %s to return\n--\n", log.ColorString("ENTER", "green"))
+						return
+					}
+					run.stacks[stklist[i]] = ""
+				}
+
+				for s := range run.stacks {
+					if err := stacks[s].GenTimeParser(); err != nil {
+						log.Error(err.Error())
+						return
+					}
+				}
+
+				// Deploy Stacks
+				stks.DeployHandler(run.stacks, stacks)
+				fmt.Printf("--\nPress %s to return\n--\n", log.ColorString("ENTER", "green"))
+				return
+			},
+		},
+
+		// terminate command
+		&ishell.Cmd{
+			Name: "terminate",
+			Help: "Terminate stacks",
+			Func: func(c *ishell.Context) {
+				// stack list
+				stklist := make([]string, len(stacks))
+				i := 0
+				for k := range stacks {
+					stklist[i] = k
+					i++
+				}
+
+				// create checklist
+				choices := c.Checklist(
+					stklist,
+					fmt.Sprintf("select stacks to %s:", log.ColorString("Terminate", "red")),
+					nil,
+				)
+
+				// define run.stacks
+				run.stacks = make(map[string]string)
+				for _, i := range choices {
+					if i < 0 {
+						fmt.Printf("--\nPress %s to return\n--\n", log.ColorString("ENTER", "green"))
+						return
+					}
+					run.stacks[stklist[i]] = ""
+				}
+
+				// Terminate Stacks
+				stks.TerminateHandler(run.stacks, stacks)
+				fmt.Printf("--\nPress %s to return\n--\n", log.ColorString("ENTER", "green"))
+				return
+
+			},
+		},
+
+		// generate command
+		&ishell.Cmd{
+			Name:     "generate",
+			Help:     "generates template from configuration values",
+			LongHelp: "generate [stack]",
+			Func: func(c *ishell.Context) {
+				var s string
+
+				if len(c.Args) > 0 {
+					s = c.Args[0]
+				}
+
+				// check if stack exists in config
+				if _, ok := stacks[s]; !ok {
+					log.Error(fmt.Sprintf("Stack [%s] not found in config", s))
+					return
+				}
+
+				if stacks[s].Source == "" {
+					log.Error("source not found in config file...")
+					return
+				}
+
+				name := fmt.Sprintf("%s-%s", project, s)
+				log.Debug(fmt.Sprintln("Generating a template for ", name))
+
+				if err := stacks[s].GenTimeParser(); err != nil {
+					log.Error(err.Error())
+					return
+				}
+				fmt.Println(stacks[s].Template)
+			},
+		},
+
+		// check command
+		&ishell.Cmd{
+			Name:     "check",
+			Help:     "validates cloudformation templates",
+			LongHelp: "check [stack]",
+			Func: func(c *ishell.Context) {
+				var s string
+
+				if len(c.Args) > 0 {
+					s = c.Args[0]
+				}
+
+				// check if stack exists in config
+				if _, ok := stacks[s]; !ok {
+					log.Error(fmt.Sprintf("Stack [%s] not found in config", s))
+					return
+				}
+
+				if stacks[s].Source == "" {
+					log.Error("source not found in config file...")
+					return
+				}
+
+				name := fmt.Sprintf("%s-%s", config.Project, s)
+				fmt.Println("Validating template for", name)
+
+				if err := stacks[s].GenTimeParser(); err != nil {
+					log.Error(err.Error())
+				}
+
+				if err := stacks[s].Check(); err != nil {
+					log.Error(err.Error())
+				}
+			},
+		},
+
+		// update command
+		&ishell.Cmd{
+			Name:     "update",
+			Help:     "updates a given stack via change-set",
+			LongHelp: "update [stack]",
+			Func: func(c *ishell.Context) {
+				var s string
+
+				if len(c.Args) < 1 {
+					log.Warn(fmt.Sprintf("Please specify stack name..."))
+					return
+				}
+
+				// define stack name
+				s = c.Args[0]
+
+				// random chcange-set name
+				run.changeName = fmt.Sprintf(
+					"%s%s",
+					s,
+					strconv.Itoa((rand.Int())),
+				)
+
+				// check if stack exists in config
+				if _, ok := stacks[s]; !ok {
+					log.Error(fmt.Sprintf("Stack [%s] not found in config", s))
+					return
+				}
+
+				if stacks[s].Source == "" {
+					log.Error("source not found in config file...")
+					return
+				}
+
+				if err := stacks[s].GenTimeParser(); err != nil {
+					log.Error(err.Error())
+					return
+				}
+
+				if err := stacks[s].Change("create", run.changeName); err != nil {
+					log.Error(err.Error())
+					return
+				}
+
+				// descrupt change-set
+				if err := stacks[s].Change("desc", run.changeName); err != nil {
+					log.Error(err.Error())
+					return
+				}
+
+				for {
+					c.Print(fmt.Sprintf(
+						"--\n%s [%s]: ",
+						log.ColorString("The above will be updated, do you want to proceed?", "red"),
+						log.ColorString("Y/N", "cyan"),
+					))
+
+					resp := c.ReadLine()
+					switch strings.ToLower(resp) {
+					case "y":
+						if err := stacks[s].Change("execute", run.changeName); err != nil {
+							log.Error(err.Error())
+							return
+						}
+						log.Info("update completed successfully...")
+						return
+					case "n":
+						if err := stacks[s].Change("rm", run.changeName); err != nil {
+							log.Error(err.Error())
+							return
+						}
+						return
+					default:
+						log.Warn(`invalid response, please type "Y" or "N"`)
+						continue
+					}
+				}
 			},
 		},
 	}
