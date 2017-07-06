eaaee7252bd230946762c92f3e49ce55c8a47696
Shaun <shaunremekie@shauns-mbp.home>
2017-07-07 00:23:26 +0100

    created bucket resolve function

--- stacks/helpers.go
+++ stacks/helpers.go
@@ -1,6 +1,10 @@
 package stacks
 
-import "fmt"
+import (
+	"fmt"
+	"qaz/bucket"
+	"time"
+)
 
 // cleanup functions in create_failed or delete_failed states
 func (s *Stack) cleanup() error {
@@ -16,4 +20,26 @@ func (s *Stack) cleanup() error {
 		}
 	}
 	return nil
+}
+
+// bucket
+func resolveBucket(s *Stack) (string, error) {
+	exists, err := bucket.Exists(s.Bucket, s.Session)
+	if err != nil {
+		Log.Warn(fmt.Sprintf("Received Error when checking if [%s] exists: %s", s.Bucket, err.Error()))
+	}
+	fmt.Println("This is test")
+	if !exists {
+		Log.Info(fmt.Sprintf(("Creating Bucket [%s]"), s.Bucket))
+		if err = bucket.Create(s.Bucket, s.Session); err != nil {
+			return "", err
+		}
+	}
+	t := time.Now()
+	tStamp := fmt.Sprintf("%d-%d-%d_%d%d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute())
+	url, err := bucket.S3write(s.Bucket, fmt.Sprintf("%s_%s.template", s.Stackname, tStamp), s.Template, s.Session)
+	if err != nil {
+		return "", err
+	}
+	return url, nil
 }
