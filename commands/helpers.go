bc2213540425700230546b8288c4f795d7c542d0
daidokoro <shaun@daidokoro.io>
2018-03-29 10:55:11 +0100

    added support for config.yaml files to be read by default if they exist

--- commands/helpers.go
+++ commands/helpers.go
@@ -2,11 +2,21 @@ package commands
 
 import "os"
 
+const (
+	defaultconfigA = "config.yml"
+	defaultconfigB = "config.yaml"
+)
+
 // DefaultConfig - sets config based on ENV variable or default config.yml
 func defaultConfig() string {
-	env := os.Getenv(configENV)
-	if env == "" {
-		return "config.yml"
+	if env := os.Getenv(configENV); env != "" {
+		return env
 	}
-	return env
+
+	if _, err := os.Stat(defaultconfigB); err == nil {
+		return defaultconfigB
+	}
+
+	return defaultconfigA
+
 }
