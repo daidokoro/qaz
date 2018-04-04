bb352a9ac4ea3b9a65554c7a0a56a98c875bbe5f
daidokoro <shaun@daidokoro.io>
2018-04-04 10:32:35 +0100

    added Protect method to stacks

---
+++ stacks/protect.go
@@ -0,0 +1,23 @@
+package stacks
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+)
+
+// Protect - enables stack termination-protection
+func (s *Stack) Protect(enable *bool) error {
+	svc := cloudformation.New(s.Session, &aws.Config{Credentials: s.creds()})
+
+	params := &cloudformation.UpdateTerminationProtectionInput{
+		EnableTerminationProtection: aws.Bool(!*enable),
+		StackName:                   &s.Stackname,
+	}
+
+	Log.Debug("Calling [UpdateTerminationProtection] with parameters:\n%s"+"\n--\n", params)
+	if _, err := svc.UpdateTerminationProtection(params); err != nil {
+		return err
+	}
+
+	return nil
+}
