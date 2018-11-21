35216d1680c9ca296ca941b31c656c668683b814
daidokoro <shaun@daidokoro.io>
2018-11-21 12:10:33 +0000

    example updated to support iteratioon

--- examples/troposphere/vpc.py
+++ examples/troposphere/vpc.py
@@ -4,37 +4,45 @@ from troposphere import Template, Ref, Tags
 class VPC(object):
     def __init__(self):
         self.template = Template()
+        
+        # using  {{ .stack.cidr }} to fetch comma separated list of 
+        # cidr blocks from qaz config
+        # and removing spaces
+        self.cidrs = [ c.strip(' ') for c in '{{ .stack.cidr }}'.split(',') ]
 
-        # Build Template
-        self.add_vpc()
-        self.add_outputs()
-
-    def add_vpc(self):
+        # iterating cidr blocks
+        for cidr in enumerate(self.cidrs):
+            self.add_vpc(cidr[0], cidr[1])
+    
+    def add_vpc(self, index, cidr):
         # Add vpc
         t = self.template
+        uid = 'VPC%s' % index
 
-        self.vpc = t.add_resource(ec2.VPC(
-            'VPC',
+        vpc = t.add_resource(ec2.VPC(
+            uid,
             # note the qaz template braces here
             # this value will be swapped out for the one in config
-            CidrBlock='{{ .stack.cidr }}',
+            CidrBlock=cidr,
             Tags=Tags(
                 Name='qaz-tropos-test'
             )
         ))
 
-    def add_outputs(self):
+        self.add_outputs(uid, vpc, desc="output for %s" % uid)
+
+    def add_outputs(self, uid, value, desc=""):
         t = self.template
 
         t.add_output(Output(
-            'vpcid',
-            Description="Vpc ID",
-            Value=Ref(self.vpc)
+            'output%s' % uid,
+            Description=desc,
+            Value=Ref(value)
         ))
 
     @staticmethod
     def template():
-        return VPC().template.to_json()
+        return VPC().template.to_yaml()
 
 # all troposphere templates must print a single
 # stack/template to stdout when run
